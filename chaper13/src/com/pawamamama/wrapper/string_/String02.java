2827af1df65c80bfa173726af4a1941c91916f7b
pawamamama <pawamamama@gmail.com>
2026-06-05 23:19:30 +0800

    chap13  -String的常量池和new对象时的创建流程-intern的使用-堆中对象value拷贝常量池的内容-编译器优化字符串拼接时的编译期间确定的字面常量-和字符串的变量拼接和使用String string3 = new StringBuilder()                                                                                                                     .append(string1)                                                                                                                     .append(string2)                                                                                                                     .toString();-和intern()一起使用时会在常量池创建

--- chaper13/src/com/pawamamama/wrapper/string_/String02.java
+++ chaper13/src/com/pawamamama/wrapper/string_/String02.java
@@ -1,10 +1,11 @@
 package com.pawamamama.wrapper.string_;
+
 /**
  * @author pawamamama
  * Date: 2026/6/5
  * 创建String象的两种方式
  * 1.直接赋值 String s = "pawamamama";
- *   先从常量池查看是否有"pawamamama"数据空间,如果有直接指向,如果没有则重新创建，然后指向。s最终指向的是常量池的空间地址
+ * 先从常量池查看是否有"pawamamama"数据空间,如果有直接指向,如果没有则重新创建，然后指向。s最终指向的是常量池的空间地址
  * 2.调用构造器 String s = "pawamamama"
  * 先在堆中创建空间里面维护了value属性指向常量池的"pawamamama"空间。
  * 如果常量池中没有hsp.重新创建，如果有直接通过value指向。最终指向的是堆中的空间地址。
@@ -13,6 +14,37 @@ package com.pawamamama.wrapper.string_;
 
 public class String02 {
     public static void main(String[] args) {
-    String s = "pawa";
+        /*String s = "pawa";
+        String s1 = "pawa";
+        System.out.println(s.equals(s1));//true
+        System.out.println(s == s1);//true
+
+
+        System.out.println(s == s1.intern());//true
+        //调用intern方法时，如果池已经包含一个等于此String对象的字符串（用equals(Object)）方法确认,则返回池中用字符串
+        //否则，将此次String对象添加到池中，并返回Stirng对象的引用
+        //false true true false true true
+        String s2 = new String("hello");
+        String s3 = new String("hello");
+        System.out.println(s2 == s3);//false，不是同一个对象，value是各自copy常量池的 针对java8
+        String s4 = s2.intern(); //intern() 会返回 常量池中已有的 "hello" 的引用
+        String s5 = "hello";
+        System.out.println(s4 == s5); // true
+        //s4 和直接写 "hello" 的 s5 引用同一个常量池对象 → == 为 true
+        String  string = "hello" +" word";
+        //编译器会优化成等价于 String  srting = "hello word";
+        //所以是在常量池里面创建了一个对象，并返回引用
+*/
+        //1.先创建一个 StringBuilder sb  = new StringBuilder();
+        //2.执行StringBuilder append(String str) 方法追加了一个 sb.append("pawa")
+        //3.再次执行StringBuilder append(String str) 方法追加了一个 sb.append("mamama")
+        //4.最后调用sb.toString 方法，value是"pawamamama";
+        //  toString里再去return new String(value, 0, count)
+        //5.Stirng = sb.toString();
+        String string1 = "pawa";
+        String string2 = "mamama";
+        String string3 = string1 + string2;
+        //这里不会创建对象，而是拼接之后创建一个在堆中的String对象value[]是pawamamama
+        // 常量池里没有 想要有就intern();方法
     }
 }
