77c616e53ce2e934c6f4e2277bc29171cb193fd7
pawamamama <pawamamama@gmail.com>
2026-06-11 23:44:28 +0800

    chapter13-feat:第一代java.util日期

---
+++ chaper13/src/com/pawamamama/wrapper/date/Dog.java
@@ -0,0 +1,43 @@
+package com.pawamamama.wrapper.date;
+
+/**
+ * Dog
+ * <pre></pre>
+ *
+ * @author pawamamama
+ * @since 2026/6/11
+ */
+public class Dog {
+    private String name;
+    private int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public void f1() {
+        int aaa = 00;
+    }
+    class  Air {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getSalary() {
+        return age;
+    }
+}
