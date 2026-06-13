1b9f13b7141edf472ad8b745a03df4d40b03e792
pawamamama <pawamamama@gmail.com>
2026-06-14 00:49:16 +0800

    chapter14-集合体系图

---
+++ chapter14/src/com/pawamamama/collection_/Collection01_.java
@@ -0,0 +1,20 @@
+package com.pawamamama.collection_;
+
+/**
+ * <pre>
+ *     集合体系图
+ *      1.集合主要是两组单列和双列
+ *      2.单列集合（集合里面方三个元素）
+ *          2.1Collection 接口有两个重要的子接口 List Set ，他们的实现子类都是单列集合
+ *      3.双列集合（）
+ *          3.1 Map 接口的实现子类 是双列集合，存放的是 K-V
+ *
+ * </pre>
+ *
+ * */
+public class Collection01_ {
+    public static void main(String[] args) {
+     //Collection 单列集合
+     //Map
+    }
+}
