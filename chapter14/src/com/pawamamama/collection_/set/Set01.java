314c345ac38d6f25b8697064459e92fd0124f6e7
pawamamama <pawamamama@gmail.com>
2026-06-18 16:25:22 +0800

    chapter14-集合collection-Set 集合基本介绍

---
+++ chapter14/src/com/pawamamama/collection_/set/Set01.java
@@ -0,0 +1,33 @@
+package com.pawamamama.collection_.set;
+
+/**
+ * Class: Set01 Set 集合基本介绍
+ *
+ * <pre>
+ * Set 接口特点：
+ * 1. 无序（元素存放顺序不保证与添加顺序一致）。
+ * 2. 不允许重复元素（由 equals() 和 hashCode() 判断）。
+ * 3. 没有索引，不能通过下标访问元素。
+ * 4. 常用遍历方式：
+ *    - Iterator（迭代器）
+ *    - 增强 for 循环（foreach）
+ * 5. 常见实现类：
+ *    - HashSet：底层基于 HashMap，查询效率高。
+ *    - LinkedHashSet：维护元素插入顺序。
+ *    - TreeSet：元素自动排序（自然排序或比较器排序）。
+ *
+ * 学习重点：
+ * - Set 为什么不能存放重复元素？
+ * - HashSet 底层如何判断重复？
+ * - hashCode() 与 equals() 的作用。
+ * - 不同 Set 实现类的底层数据结构及特点。
+ * </pre>
+ *
+ * @author pawamamama
+ * @date 2026/6/18
+ */
+public class Set01 {
+    public void main(String[] args) {
+
+    }
+}
