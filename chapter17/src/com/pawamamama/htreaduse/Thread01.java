d74902ec6c0862a82281719b342edcd688b4aaab
pawamamama <pawamamama@gmail.com>
2026-07-08 21:43:48 +0800

    chapter17-多线程初级-继承Thread创建线程-start简单分析-为什么不直接cat.run()运行线程

--- chapter17/src/com/pawamamama/htreaduse/Thread01.java
+++ chapter17/src/com/pawamamama/htreaduse/Thread01.java
@@ -107,7 +107,21 @@ public class Thread01 {
     public static void main(String[] args) {
         //创建Cat对象，当作线程使用
         Cat cat = new Cat();
-        cat.start();//启动线程
+        //start源码部分
+        /*    (1)
+                public synchronized void start() {
+                        start0();
+                }
+              (2)start0是一个本地方法，是JVM调用，底层是c/c++实现
+                //真正实现多线程的效果，是start0方法，而不是run
+                private native void start0();
+                //该线程不会里面执行，只是将线程变成了可运行状态，具体什么时候执行，取决于cpu
+                //由cpu同一调度
+
+        *
+        * */
+        cat.start();//启动线程->最终执行cat的run方法
+        //cat.run();//run方法就是一个普通的run方法，并没有真正的启动线程，就会把run方法执行完毕，才向下执行
         //当 main 启动一个子线程后 Thread - 0 ,主线程不会阻塞，会继续执行
         //这时主线程和子线程是交替执行的
         System.out.println("主线程继续执行" + Thread.currentThread().getName());//名字默认叫 - main
