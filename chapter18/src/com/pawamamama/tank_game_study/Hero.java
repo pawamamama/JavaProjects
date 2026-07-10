814d9759f2bef2e69f84d3c7fe3b60c45393eb7f
pawamamama <pawamamama@gmail.com>
2026-07-10 22:28:59 +0800

    chapter18-坦克大战2（study + teacher）: 为hero 添加射击线程-子弹线程创建和坐标初始化

--- chapter18/src/com/pawamamama/tank_game_study/Hero.java
+++ chapter18/src/com/pawamamama/tank_game_study/Hero.java
@@ -11,7 +11,31 @@ package com.pawamamama.tank_game_study;
  */
 @SuppressWarnings({"all"})
 public class Hero extends Tank{
+    //shot对象
+    Shot shot= null;
     public Hero(int x, int y) {
         super(x, y);
+    }
+    //子弹射击方法
+    public void shotEnemyTank() {
+        switch (getDirect()) {
+            case 0: // 向上
+                shot = new Shot(getX() + 20, getY(), 0);
+                break;
+
+            case 1: // 向右
+                shot = new Shot(getX() + 60, getY() + 20, 1);
+                break;
+
+            case 2: // 向下
+                shot = new Shot(getX() + 20, getY() + 60, 2);
+                break;
+
+            case 3: // 向左
+                shot = new Shot(getX(), getY() + 20, 3);
+                break;
+        }
+        Thread shotThread = new Thread(shot);
+        shotThread.start();
     }
 }
--- chapter18/src/com/pawamamama/tank_game_teacher/Hero.java
+++ chapter18/src/com/pawamamama/tank_game_teacher/Hero.java
@@ -1,3 +1,4 @@
+
 package com.pawamamama.tank_game_teacher;
 
 /**
@@ -11,8 +12,38 @@ package com.pawamamama.tank_game_teacher;
  * @date 2026/7/6
  */
 @SuppressWarnings({"all"})
-public class Hero  extends Tank{
+public class Hero extends Tank {
+    //定义一个Shot对象，表示一个射击行为(线程)
+    Shot shot = null;
+
     public Hero(int x, int y) {
         super(x, y);
+    }
+
+    //射击
+    public void shotEnemyTank() {
+        //真正创建Shot对象
+        //1.创建对象时，要根据当前Hero对象的位置和方向来创建
+        switch (getDirect()) {
+
+            case 0: // 向上
+                shot = new Shot(getX() + 20, getY(), 0);
+                break;
+
+            case 1: // 向右
+                shot = new Shot(getX() + 60, getY() + 20, 1);
+                break;
+
+            case 2: // 向下
+                shot = new Shot(getX() + 20, getY() + 60, 2);
+                break;
+
+            case 3: // 向左
+                shot = new Shot(getX(), getY() + 20, 3);
+                break;
+        }
+        //启动射击线程
+        Thread thread = new Thread(shot);
+        thread.start();
     }
 }
