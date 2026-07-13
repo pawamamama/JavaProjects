f945398cc38809f617df4c7e90f5f598d6399569
pawamamama <pawamamama@gmail.com>
2026-07-13 21:57:28 +0800

    chapter18-坦克大战2(0.4版)（study + teacher）:-敌人移动发射子弹

--- chapter18/src/com/pawamamama/tank_game_study/EnemyTank.java
+++ chapter18/src/com/pawamamama/tank_game_study/EnemyTank.java
@@ -19,15 +19,48 @@ public class EnemyTank extends Tank implements Runnable {
     int sleep = 100;
     //敌方子弹线程
     Vector<Shot> shots = new Vector();
+    //子弹最大值
+    int bulletBigSize = 5;
 
     public EnemyTank(int x, int y) {
         super(x, y);
         setSpeed(3);
     }
+    //敌人发射子弹
+    private void shotHero() {
+        //临时变量
+        Shot shot = null;
+        switch (getDirect()) {
+
+            case 0: // 向上
+                shot = new Shot(getX() + 20, getY(), 0);
+                break;
+
+            case 1: // 向右
+                shot = new Shot(getX() + 60, getY() + 20, 1);
+                break;
+
+            case 2: // 向下
+                shot = new Shot(getX() + 20, getY() + 60, 2);
+                break;
+
+            case 3: // 向左
+                shot = new Shot(getX(), getY() + 20, 3);
+                break;
+        }
+        //把shot加入到集合中
+        shots.add(shot);
+        //启动射击线程
+        Thread thread = new Thread(shot);
+        thread.start();
+    }
 
     @Override
     public void run() {
         while (true) {
+            if (isLive && shots.size() < bulletBigSize ) {
+                shotHero();
+            }
             //根据坦克的方向来继续移动
             switch (getDirect()) {
                 case 0://上
--- chapter18/src/com/pawamamama/tank_game_teacher/EnemyTank.java
+++ chapter18/src/com/pawamamama/tank_game_teacher/EnemyTank.java
@@ -26,19 +26,57 @@ public class EnemyTank extends Tank implements Runnable {
     int sleep = 100;
     //在敌人坦克类，使用Vector 保持多个Shot
     Vector<Shot> shots = new Vector();
+    //敌人坦克发射子弹最大值
+    int bulletBigSize = 5;
+
 
     public EnemyTank(int x, int y) {
 
         super(x, y);
         setSpeed(3);
     }
+
+    //发射子弹方法
+    private void shotHero() {
+        //临时变量
+        Shot shot = null;
+        switch (getDirect()) {
+
+            case 0: // 向上
+                shot = new Shot(getX() + 20, getY(), 0);
+                break;
+
+            case 1: // 向右
+                shot = new Shot(getX() + 60, getY() + 20, 1);
+                break;
+
+            case 2: // 向下
+                shot = new Shot(getX() + 20, getY() + 60, 2);
+                break;
+
+            case 3: // 向左
+                shot = new Shot(getX(), getY() + 20, 3);
+                break;
+        }
+        //把shot加入到集合中
+        shots.add(shot);
+        //启动射击线程
+        Thread thread = new Thread(shot);
+        thread.start();
+    }
+
     @Override
     public void run() {
         while (true) {
+            //如果shots size() < 5 && isLive == true
+            if (shots.size() < 5 && isLive) {
+                //就发射子弹
+                shotHero();
+            }
             //根据坦克的方向来继续移动
             switch (getDirect()) {
                 case 0://上
-                    for (int i = 0; i <go ; i++) {
+                    for (int i = 0; i < go; i++) {
                         moveUp();
 
                         try {
@@ -49,7 +87,7 @@ public class EnemyTank extends Tank implements Runnable {
                     }
                     break;
                 case 1:
-                    for (int i = 0; i <go ; i++) {
+                    for (int i = 0; i < go; i++) {
                         moveRight();
 
                         try {
@@ -60,24 +98,26 @@ public class EnemyTank extends Tank implements Runnable {
                     }
                     break;
                 case 2:
-                    for (int i = 0; i <go ; i++) {
+                    for (int i = 0; i < go; i++) {
                         moveDown();
                         try {
                             Thread.sleep(sleep);
-                        } catch (InterruptedException e) {}
+                        } catch (InterruptedException e) {
+                        }
                     }
                     break;
                 case 3:
-                    for (int i = 0; i <go ; i++) {
+                    for (int i = 0; i < go; i++) {
                         moveLeft();
                         try {
                             Thread.sleep(sleep);
-                        } catch (InterruptedException e) {}
+                        } catch (InterruptedException e) {
+                        }
                     }
                     break;
             }
             //然后随机的改变坦克方向 0~3的整数
-            setDirect((int)(Math.random() * 4));
+            setDirect((int) (Math.random() * 4));
             //写多线程并发程序一定要考虑线程什么时候结束
             if ((!isLive)) {//死了就退出线程
                 break;
