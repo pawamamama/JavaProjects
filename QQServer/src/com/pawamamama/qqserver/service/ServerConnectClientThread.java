40614cf252fbb211ba7f170c34fd5b37fa1419c5
pawamamama <pawamamama@gmail.com>
2026-09-25 20:15:04 +0800

    QQ网络项目-服务端-socket线程类-添加相应式代码-返回用户列表

--- QQServer/src/com/pawamamama/qqserver/service/ServerConnectClientThread.java
+++ QQServer/src/com/pawamamama/qqserver/service/ServerConnectClientThread.java
@@ -1,9 +1,11 @@
 package com.pawamamama.qqserver.service;
 
 import com.pawamamama.qqcommon.Message;
+import com.pawamamama.qqcommon.MessageType;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 /**
@@ -32,7 +34,24 @@ public class ServerConnectClientThread extends Thread {
                 ObjectInputStream ois =
                         new ObjectInputStream(socket.getInputStream());
                 Message message = (Message) ois.readObject();
-                //后面会使用
+                //后面会使用message 根据类型做处理
+                if (message.getMesType().equals(MessageType.MESSAGE_GET_ONLINE_FRIEND)) {
+                    //客户端要在线用户列表
+                    System.out.println(message.getSender() + " 该用户要在线用户列表");
+                    final String onlineUser = ManageClientThreads.getOnlineUser();
+                    //准备发送给服务器，用message返回
+                    final Message message1 = new Message();
+                    //客户端要检查类型，设置成
+                    message1.setMesType(MessageType.MESSAGE_RET_ONLINE_FRIEND);
+                    message1.setContent(onlineUser);//信息写入
+                    //设置接收者是谁，是上面回信的消息的发送者
+                    message1.setGetter(message.getSender());
+                    //写入到数据通道
+                    final ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+                    oos.writeObject(message1);
+                }else {
+                    System.out.println("其他类型的message");
+                }
              } catch (Exception e) {
                 e.printStackTrace();
             }
