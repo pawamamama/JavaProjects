7f0436e6a53f35c656fabbfd09158a116dcc04c8
pawamamama <pawamamama@gmail.com>
2026-09-26 22:42:34 +0800

    QQ网络项目-客户端-私聊实现-重构日期匹配格式

--- QQClient/src/com/pawamamama/qqclient/service/MessageClientService.java
+++ QQClient/src/com/pawamamama/qqclient/service/MessageClientService.java
@@ -1,9 +1,11 @@
 package com.pawamamama.qqclient.service;
 
 import com.pawamamama.qqcommon.Message;
+import com.pawamamama.qqcommon.MessageType;
 
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 /**
  * Class: MessageClientService
@@ -28,10 +30,14 @@ public class MessageClientService {
     public void sendMessageToOne(String content,String senderId,String getterId) {
         //构建message
         final Message message = new Message();
+        //设置类型
+        message.setMesType(MessageType.MESSAGE_COMM_MES);//普通私聊
         message.setSender(senderId);
         message.setGetter(getterId);
         message.setContent(content);
-        message.setSendTime(new Date().toString());//发送时间设置
+        //时间格式化
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        message.setSendTime(sdf.format(new Date()));
         System.out.println(senderId + "对 " + getterId+"说: " + content);
         //发送给服务端
         //拿到socket
